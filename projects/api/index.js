91c252663d291ef5aea4068506c2fd0fa396b356
Ricardo Castanho <48383095+ricardocastanho@users.noreply.github.com>
2023-01-04 19:56:39 +0000

    feat: persist inferred schema

--- projects/api/index.js
+++ projects/api/index.js
@@ -1,32 +1,47 @@
-const { makeAugmentedSchema, inferSchema } = require("neo4j-graphql-js");
+const fs = require("fs");
 const neo4j = require("neo4j-driver");
+const { makeAugmentedSchema } = require("neo4j-graphql-js");
 const { ApolloServer } = require("apollo-server");
 require("dotenv").config({ path: __dirname + "/../../.env" });
+
+// const inferAugmentedSchema = driver => {
+//   return inferSchema(driver).then(result => {
+//     return makeAugmentedSchema({
+//       typeDefs: result.typeDefs
+//     });
+//   });
+// };
+
+// const persistInferredSchema = driver => {
+//   const schemaInferenceOptions = {
+//     alwaysIncludeRelationships: false
+//   };
+//   
+//   return inferSchema(driver, schemaInferenceOptions).then((result) => {
+//     fs.writeFile("schema.graphql", result.typeDefs, (err) => {
+//       if (err) throw err;
+//       console.log("Updated schema.graphql");
+//       process.exit(0);
+//     });
+//   });
+// }
 
 const driver = neo4j.driver(
   `${process.env.DB_HOST}:${process.env.DB_PORT}`,
   neo4j.auth.basic(process.env.DB_USER, process.env.DB_PASSWORD)
 );
 
-const inferAugmentedSchema = driver => {
-  return inferSchema(driver).then(result => {
-    return makeAugmentedSchema({
-      typeDefs: result.typeDefs
-    });
-  });
-};
+const typeDefs = fs
+  .readFileSync(__dirname + "/schema.graphql")
+  .toString("utf-8");
 
-const createServer = schema => {
-  return new ApolloServer({
-    schema,
-    context: { driver }
-  });
-};
+const schema = makeAugmentedSchema({ typeDefs });
 
-inferAugmentedSchema(driver)
-  .then(createServer)
-  .then(server => server.listen(3003, '0.0.0.0'))
-  .then(({ url }) => {
-    console.log(`GraphQL API ready at ${url}`);
-  })
-  .catch(err => console.error(err));
+const server = new ApolloServer({
+  schema,
+  context: { driver },
+});
+
+server.listen(3003, "0.0.0.0").then(({ url }) => {
+  console.log(`GraphQL API ready at ${url}`);
+});
