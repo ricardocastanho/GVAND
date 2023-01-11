bb3440299f1ecc79d516891846bc2cc8f48b34d5
Ricardo Castanho <48383095+ricardocastanho@users.noreply.github.com>
2023-01-11 16:42:52 +0000

    refactor: move helpers

--- projects/client/src/helpers/index.js
+++ projects/client/src/helpers/index.js
@@ -1,15 +1,1 @@
-export const greetUser = name => {
-  const hour = new Date().getHours()
-
-  if (hour >= 6 && hour <= 12) {
-    return `Bom dia, ${name}!`
-  }
-
-  if (hour > 12 && hour <= 19) {
-    return `Boa tarde, ${name}!`
-  }
-
-  if (hour > 19 && hour < 6) {
-    return `Boa noite, ${name}!`
-  }
-}
+export * from './User';
