c69aa067bb91b73f820d838322b3782758304f19
Ricardo Castanho <48383095+ricardocastanho@users.noreply.github.com>
2023-01-13 20:24:40 +0000

    feat: add a carousel component to load movies

---
+++ projects/client/src/components/MovieCarousel.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-carousel
+    cycle
+    height="600"
+    dark
+    hide-delimiter-background
+    show-arrows-on-hover
+  >
+    <v-carousel-item
+      v-for="(movie, i) in movies"
+      :key="i"
+    >
+      <v-img
+        height="100%"
+        width="100%"
+        :src="movie.banner"
+        class="carousel-image"
+      />
+      <v-card-title class="carousel-title text-h2 font-weight-medium">
+        {{ movie.title }}
+      </v-card-title>
+      <v-card-text class="carousel-subtitle text-h6 text-subtitle-1 d-inline-block text-truncate">
+        {{ movie.plot }}
+      </v-card-text>
+    </v-carousel-item>
+  </v-carousel>
+</template>
+
+<script>
+import { movies } from '@/mock.json'
+
+export default {
+  name: 'MovieCarousel',
+  data () {
+    return {
+      movies
+    }
+  },
+}
+</script>
+
+<style scoped>
+.carousel-image {
+  filter: brightness(50%);
+}
+
+.carousel-title {
+  position: absolute;
+  bottom: 70px;
+  left: 16px;
+}
+
+.carousel-subtitle {
+  position: absolute;
+  bottom: 30px;
+  left: 16px;
+  max-height: 80px;
+  max-width: 50%;
+}
+</style>
