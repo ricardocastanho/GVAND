748f4b5e2f873a376be392303868e7a1db499bc2
Ricardo Castanho <48383095+ricardocastanho@users.noreply.github.com>
2023-01-17 19:36:45 +0000

    feat: improve router history mode

--- projects/client/src/plugins/router.js
+++ projects/client/src/plugins/router.js
@@ -6,7 +6,7 @@ import routes from '../routes'
 Vue.use(VueRouter);
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: import.meta.env.MODE === 'development' ? 'hash' : 'history',
   routes
 });
 
